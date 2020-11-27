a47afa08b592ad4a175f239799ca0260d83b5f9d
Ryohji Ikebe <Ryohji.Ikebe@sony.com>
2020-11-27 19:26:09 +0900

    initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace Gzip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = new GzipCompressionProvider(new GzipCompressionProviderOptions());
+            var stream = provider.CreateStream(System.Console.OpenStandardOutput());
+            stream.Write(Encoding.UTF8.GetBytes("hello world."));
+            stream.Close();
+        }
+    }
+}
